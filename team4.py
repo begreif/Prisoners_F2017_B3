b0aa80f4f5cdfe3e84e9c2577af0ee9c9c0baee7
begreif <begreif@student.linnmar.org>
2016-10-06 12:36:25 -0500

    Created our strategy
    RandomStrategy

--- team4.py
+++ team4.py
@@ -1,3 +1,5 @@
+import random
+#Ben G and Ben L
 ####
 # Each team's file must define four tokens:
 #     team_name: a string
@@ -6,9 +8,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Meteor2016' # Only 10 chars displayed.
+strategy_name = 'RandomStrategy'
+strategy_description = 'Makes decisions based on opponents history'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -17,17 +19,20 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
-
+    if their_history == '': 
+        return 'b'
+    if my_history == 'b':
+        return 'c'
+    if 'cbcbcbcbcb' in their_history:
+        return 'b' 
+    if their_history[-2:] == 'bb':
+        return 'b'
+    if their_history[-2:] == 'bc':
+        return random.choice('bc')
+    if their_history[-2:] == 'cb':
+        return random.choice('bc')
+    if their_history[-2:] == 'cc':
+        return 'b'
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
